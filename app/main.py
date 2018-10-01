c3249c1ab650bdff76856eca88f46fc7552612a7
JP <janis.puris@gmail.com>
2018-10-01 09:31:08 +0200

    init

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, flash, redirect, render_template, request, session, abort
+
 app = Flask(__name__)
  
 @app.route("/")
@@ -6,6 +7,8 @@ def welcome():
     return render_template('app.html')
  
 if __name__ == "__main__":
+
     app.jinja_env.auto_reload = True
     app.config['TEMPLATES_AUTO_RELOAD'] = True
+
     app.run(app.run(host='0.0.0.0', port=5000))
